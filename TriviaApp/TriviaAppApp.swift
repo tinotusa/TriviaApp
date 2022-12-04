f29374b3a47d531d263156f8be3b29df7e975f2a
Tino Tusaumwe <tusatino@gmail.com>
2022-12-04 20:58:56 +1000

    Initial commit

---
+++ TriviaApp/TriviaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaAppApp.swift
+//  TriviaApp
+//
+//  Created by Tino on 4/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TriviaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
