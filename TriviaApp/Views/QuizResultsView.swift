5108f464711640aa5d932835e076278e08b6f965
Tino Tusaumwe <tusatino@gmail.com>
2022-12-14 13:24:02 +1000

    Add QuizResultsView

---
+++ TriviaApp/Views/QuizResultsView.swift
@@ -0,0 +1,20 @@
+//
+//  QuizResultsView.swift
+//  TriviaApp
+//
+//  Created by Tino on 14/12/2022.
+//
+
+import SwiftUI
+
+struct QuizResultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuizResultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuizResultsView()
+    }
+}
