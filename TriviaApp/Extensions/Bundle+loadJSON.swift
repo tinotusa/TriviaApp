b1d535c16d7bb11725c3a9670d589feaa2d834e3
Tino Tusaumwe <tusatino@gmail.com>
2022-12-14 14:19:15 +1000

    Add loadJSON extension to Bundle

--- TriviaApp/Extensions/Bundle+loadJSON.swift
+++ TriviaApp/Extensions/Bundle+loadJSON.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+extension Bundle {
+    /// Loads json from the main Bundle.
+    /// - Parameter filename: The file name.
+    /// - Returns: The decoded type of the json.
+    func loadJSON<T: Codable>(filename: String) -> T {
+        guard let url = Bundle.main.url(forResource: filename, withExtension: "json") else {
+            fatalError("File named \"\(filename)\" couldn't be found.")
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            fatalError("Failed to decode data. \(error)")
+        }
+    }
+}
