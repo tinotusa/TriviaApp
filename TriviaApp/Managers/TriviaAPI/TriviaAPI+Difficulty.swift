9ecf5530f81ea89687d695926ce38887e92a819a
Tino Tusaumwe <tusatino@gmail.com>
2022-12-15 11:56:43 +1000

    Add title property and document comments

--- TriviaApp/Managers/TriviaAPI/TriviaAPI+Difficulty.swift
+++ TriviaApp/Managers/TriviaAPI/TriviaAPI+Difficulty.swift
@@ -6,14 +6,27 @@
 //
 
 import Foundation
+import SwiftUI
+
 extension TriviaAPI {
+    /// Difficulty options for the trivia.
     enum TriviaDifficulty: String, CaseIterable, Identifiable {
         case any
         case easy
         case medium
         case hard
         
+        /// A unique id.
         var id: Self { self }
+        
+        /// The title of the case.
+        var title: LocalizedStringKey {
+            switch self {
+            case .any: return LocalizedStringKey("Any")
+            case .easy: return LocalizedStringKey("Easy")
+            case .hard: return LocalizedStringKey("Hard")
+            case .medium: return LocalizedStringKey("Medium")
+            }
+        }
     }
-    
 }
