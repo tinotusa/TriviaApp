fe343bcb422f38d8519ddad3bcd5f0a3f0d507c0
Tino Tusaumwe <tusatino@gmail.com>
2022-12-17 15:11:41 +1000

    Fix Info.plist bug
    Fixed a warning that would show "Info.plist contained no UIScene configuration
    dictionary". This seems to have been created by updating xcode.

---
+++ TriviaApp/Views/CreditsView.swift
@@ -0,0 +1,20 @@
+//
+//  CreditsView.swift
+//  TriviaApp
+//
+//  Created by Tino on 17/12/2022.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+    }
+}
