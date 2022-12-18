da59b671b7184cd0ed210adf5a75d5f86c7ce7e4
Tino Tusaumwe <tusatino@gmail.com>
2022-12-18 12:04:52 +1000

    Add colours to assets

---
+++ TriviaApp/Extensions/Color+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Color+Extensions.swift
+//  TriviaApp
+//
+//  Created by Tino on 18/12/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let answerButton = Color("answerButton")
+    static let answerButtonStroke = Color("answerButtonStroke")
+    static let background = Color("background")
+    static let darkGray = Color("darkGray")
+    static let darkGreen = Color("darkGreen")
+    static let darkYellow = Color("darkYellow")
+    static let gray = Color("gray")
+    static let green = Color("green")
+    static let text = Color("text")
+    static let yellow = Color("yellow")
+}
