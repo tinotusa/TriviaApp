2ee7eb454c0548e706734a5bc95c1f817fb110fe
Tino Tusaumwe <tusatino@gmail.com>
2022-12-18 23:13:53 +1000

    Add ErrorView UI

--- TriviaApp/Views/ErrorView.swift
+++ TriviaApp/Views/ErrorView.swift
@@ -7,14 +7,56 @@
 
 import SwiftUI
 
-struct ErrorView: View {
+struct ErrorView<Content: View>: View {
+    let title: String
+    var detail: String?
+    var actions: (() -> Content)
+    
+    init(title: String, detail: String? = nil, @ViewBuilder actions: @escaping (() -> Content)) {
+        self.title = title
+        self.detail = detail
+        self.actions = actions
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "xmark.circle")
+                .foregroundColor(.red)
+                .font(.title)
+            
+            Text(title)
+            
+            if let detail {
+                Divider()
+                Text(detail)
+                    .multilineTextAlignment(.center)
+            }
+            
+            Divider()
+            
+            actions()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.background)
+        .bodyStyle()
     }
 }
 
 struct ErrorView_Previews: PreviewProvider {
     static var previews: some View {
-        ErrorView()
+        ErrorView(
+            title: "Something went wrong.",
+            detail: "this is the detail message"
+        ) {
+            VStack {
+                Button("one") {
+                    
+                }
+                Button("two") {
+                    
+                }
+            }
+        }
     }
 }
