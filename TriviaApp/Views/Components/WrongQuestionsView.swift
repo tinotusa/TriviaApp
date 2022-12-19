e45086c6eb8427b3cf0e8b40513ac61523a81791
Tino Tusaumwe <tusatino@gmail.com>
2022-12-19 19:46:27 +1000

    Add WrongQuestionsView

---
+++ TriviaApp/Views/Components/WrongQuestionsView.swift
@@ -0,0 +1,59 @@
+//
+//  WrongQuestionsView.swift
+//  TriviaApp
+//
+//  Created by Tino on 19/12/2022.
+//
+
+import SwiftUI
+
+/// A list of the questions the user answered incorrectly.
+///
+/// This is shown in the QuizResultsView
+struct WrongQuestionsView: View {
+    /// The incorrectly answered questions.
+    var questions: [TriviaQuestion]
+    
+    var body: some View {
+        VStack {
+            Text("Questions you got wrong.")
+                .mediumBodyStyle()
+            
+            ViewThatFits(in: .vertical) {
+                listView
+                
+                GeometryReader { proxy in
+                    ScrollView(showsIndicators: false) {
+                        listView
+                            .frame(maxWidth: proxy.size.width)
+                            .frame(minHeight: proxy.size.height)
+                    }
+                }
+            }
+        }
+        .frame(maxHeight: .infinity)
+    }
+}
+
+private extension WrongQuestionsView {
+    var listView: some View {
+        VStack {
+            ForEach(questions, id: \.self) { question in
+                VStack(alignment: .leading) {
+                    Text("Q: \(question.question)")
+                        .padding(.bottom, 3)
+                    Text("A: \(question.correctAnswer)")
+                    Divider()
+                }
+            }
+        }
+    }
+}
+
+struct WrongQuestionsView_Previews: PreviewProvider {
+    static let questions = Array(Bundle.main.loadJSON(QuestionsResponse.self, filename: "exampleQuestions").results[0..<3])
+    
+    static var previews: some View {
+        WrongQuestionsView(questions: questions)
+    }
+}
