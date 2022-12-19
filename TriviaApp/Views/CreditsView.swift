368b5357ce3265b7db0002ad77ebd1574d124b55
Tino Tusaumwe <tusatino@gmail.com>
2022-12-19 19:55:08 +1000

    Add CreditsView UI

--- TriviaApp/Views/CreditsView.swift
+++ TriviaApp/Views/CreditsView.swift
@@ -11,25 +11,31 @@ struct CreditsView: View {
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        NavigationStack {
+        VStack(alignment: .leading) {
+            HStack {
+                Button("Close") {
+                    dismiss()
+                }
+                .foregroundColor(.blue)
+                
+                Spacer()
+            }
+            
             ScrollView {
                 VStack(alignment: .leading) {
+                    Text("Acknowledgements")
+                        .modifier(TextStyle(font: .sfCompactRoundedMedium, fontSize: 35, color: .text))
+                        .minimumScaleFactor(0.7)
+                    
                     creditRow(url: URL(string: "https://opentdb.com")!)
                     creditRow(url: URL(string: "https://opengameart.org")!)
                     creditRow(url: URL(string: "https://pixabay.com")!)
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .navigationTitle("Acknowledgements")
-                .toolbar {
-                    ToolbarItem(placement: .cancellationAction) {
-                        Button("Close") {
-                            dismiss()
-                        }
-                    }
-                }
-                .padding()
             }
         }
+        .bodyStyle()
+        .padding()
+        .background(Color.background)
     }
 }
 
@@ -39,6 +45,7 @@ private extension CreditsView {
     /// - Returns: A link view.
     func creditRow(url: URL) -> some View {
         Link(linkName(url: url), destination: url)
+            .foregroundColor(.blue)
     }
     
     /// The host name of a given URL.
