7b410093dff3800a27b7fbec798e2f6b5148fa4f
Tino Tusaumwe <tusatino@gmail.com>
2022-12-22 12:38:39 +1000

    Add HomeView UI tests

--- TriviaAppUITests/HomeViewUITests.swift
+++ TriviaAppUITests/HomeViewUITests.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 final class HomeViewUITests: XCTestCase {
-    let app = TriviaAppApp()
+    let app = XCUIApplication()
     
     override func setUpWithError() throws {
         continueAfterFailure = false
@@ -19,16 +19,91 @@ final class HomeViewUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testSettingTriviaProperties() throws {
-        try app.triviaCategoryPicker().select(.art)
-        try app.triviaCategoryPicker().select(.videoGames)
-        for _ in 0 ..< 5 {
-            try app.numberOfQuestionsButton(.increment).tap()
+    enum Category: String {
+        case art = "Art"
+        case videoGames = "Video games"
+    }
+    
+    func testSetTriviaCategory() throws {
+        let categoryPicker = app.buttons["Category Picker"]
+        let category = Category.art.rawValue
+        
+        XCTAssertTrue(categoryPicker.exists, "Expected the category picker to exist.")
+        categoryPicker.tap()
+        
+        let artButton = app.buttons[category]
+        artButton.tap()
+        
+        XCTAssertTrue(app.staticTexts[category].exists, "Expected art category to be set.")
+    }
+    
+    func testIncrementButton() throws {
+        let incrementButton = app.buttons["Stepper increment"]
+        XCTAssertTrue(incrementButton.exists, "Expected the increment button to exist.")
+        
+        let stepperTextField = app.textFields["Stepper textField"]
+        let textFieldValue = try XCTUnwrap(stepperTextField.value as? String, "Expected to unwrap stepper textfield value.")
+        let value = try XCTUnwrap(Int(textFieldValue), "Expected to unwrap text field value to Int.")
+        
+        let tapCount = 5
+        let expectedValue = "\(value + tapCount)"
+        for _ in 0 ..< tapCount {
+            incrementButton.tap()
         }
+        
+        XCTAssertEqual(stepperTextField.value as! String, expectedValue, "Expected stepper text field value to be \(expectedValue)")
+    }
+    
+    func testDecrementButton() throws {
+        let decrementButton = app.buttons["Stepper decrement"]
+        XCTAssertTrue(decrementButton.exists, "Expected the increment button to exist.")
+        let stepperTextField = app.textFields["Stepper textField"]
+
         for _ in 0 ..< 5 {
-            try app.numberOfQuestionsButton(.decrement).tap()
+            decrementButton.tap()
         }
-        try app.difficultyPicker().select(.hard)
-        try app.questionTypePicker().select(.any)
+        let expectedValue = "5"
+        XCTAssertEqual(stepperTextField.value as! String, expectedValue, "Expected stepper text field value to be \(expectedValue).")
+    }
+    
+    func testSetNumberOfQuestionsWithKeyboard() throws {
+        let stepperTextField = app.textFields["Stepper textField"]
+        XCTAssertTrue(stepperTextField.exists, "Expected the stepper text field to exist.")
+        let currentValue = stepperTextField.value as! String
+        
+        let value = "15"
+        
+        // this taps the end of the text field
+        let test = stepperTextField.coordinate(withNormalizedOffset: CGVector(dx: 0.9, dy: 0.9))
+        test.tap()
+//        stepperTextField.tap()
+        
+        stepperTextField.typeText(String(repeating: XCUIKeyboardKey.delete.rawValue, count: currentValue.count))
+        stepperTextField.typeText(value)
+        XCTAssertEqual(stepperTextField.value as! String, value, "Expected stepper text field value to be \(value)")
+    }
+
+    enum QuestionType: String {
+        case any = "Any"
+        case multipleChoice = "Multiple choice"
+        case trueOrFalse = "True or false"
+    }
+    
+    func testSetQuestionType() throws {
+        let questionTypePicker = app.buttons["Question type picker"]
+        questionTypePicker.tap()
+        
+        let questionType = QuestionType.multipleChoice.rawValue
+        let typeButton = app.buttons[questionType]
+        XCTAssertTrue(typeButton.exists, "Expected the \(questionType) button to exist.")
+        
+        typeButton.tap()
+        let questionTypeExists = app.staticTexts[questionType].exists
+        XCTAssertTrue(questionTypeExists, "Expected \(questionType) to be set.")
+    }
+    
+    func testStartTriviaButton() {
+        let startButton = app.buttons["Start Trivia"]
+        XCTAssertTrue(startButton.isEnabled, "Expected the start trivia button to be enabled.")
     }
 }
