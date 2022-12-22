4761834d9cf416c4020695699c5400684fde4f63
Tino Tusaumwe <tusatino@gmail.com>
2022-12-22 20:00:03 +1000

    Add clamp function to score property

--- TriviaApp/Models/TriviaResult.swift
+++ TriviaApp/Models/TriviaResult.swift
@@ -10,12 +10,25 @@ import Foundation
 /// Results of a particular trivia round.
 struct TriviaResult {
     /// The amount of correctly answered questions
-    var score: Int = 0
+    var score: Int {
+        didSet {
+            score = clamp(score: score)
+        }
+    }
     /// All the questions that were asked.
     var questions: [TriviaQuestion]
     /// The questions that were answered incorrectly.
     var wrongQuestions: [TriviaQuestion] = []
     
+    init(score: Int = 0, questions: [TriviaQuestion], wrongQuestions: [TriviaQuestion] = []) {
+        self.score = score
+        self.questions = questions
+        self.wrongQuestions = wrongQuestions
+        self.score = clamp(score: score)
+    }
+}
+
+extension TriviaResult {
     /// The score as a percentage.
     var percentage: Double {
         if score == 0 { return 0 }
@@ -25,5 +38,21 @@ struct TriviaResult {
     /// A boolean value indicating whether the trivia round had any wrong answers.
     var isPerfectScore: Bool {
         score == questions.count
+    }
+    
+    /// Limits the given value between a min and max value.
+    ///
+    /// 0 if `score` < 0
+    ///
+    /// questions.score if `score` > questions.score
+    ///
+    /// The value unchanged.
+    ///
+    /// - Parameter score: The score to clamp.
+    /// - Returns: The clamped value
+    private func clamp(score: Int) -> Int {
+        if score < 0 { return 0 }
+        else if score > questions.count { return questions.count }
+        return score
     }
 }
